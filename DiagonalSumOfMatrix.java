f5bf13704532697811157cdac0cc585692f37bd7
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-05-15 10:25:16 +0530

    Update DiagonalSumOfMatrix.java

--- DiagonalSumOfMatrix.java
+++ DiagonalSumOfMatrix.java
@@ -17,12 +17,58 @@ public class DiagonalSumOfMatrix{
 		System.out.println("\nEnter the elements : ");
 		takeInput(list,rows,columns,sc);
 
+		System.out.println("\nResulted Matrix");
+		System.out.println("~~~~~~~~~~~~~~~~~~~");
+		printListInMatrixForm(list);
+
+		System.out.println("\nDiagonal Of Matrix");
+		System.out.println("~~~~~~~~~~~~~~~~~~~");
+		int diagonalSum = findDiagonalSumAndPrintDiagonalOfMatrix(list,rows,columns);
+
+		System.out.println("\nDiagonal Sum : "+diagonalSum);
+
+		sc.close();
 
 	}
 	public static void takeInput(List<List<Integer>> list,int rows,int columns,Scanner sc){
 
-		for (int i = 0 ;  i < ; ) {
-			
+		for (int i = 0 ;  i < rows; ++i) {
+			List<Integer> row = new ArrayList<>();
+			for (int j = 0 ; j < columns ; ++j) {
+				
+				int element = sc.nextInt();
+				row.add(element);
+			}
+			list.add(row);
 		}
+	}
+	public static void printListInMatrixForm(List<List<Integer>> list){
+
+		for (List<Integer> row : list) {
+			
+			for (int item : row) {
+				System.out.print(item+"\t");
+			}
+			System.out.println();
+		}
+	}
+	public static int findDiagonalSumAndPrintDiagonalOfMatrix(List<List<Integer>> list,int rows,int columns){
+
+		int diagonalSum = 0;
+
+		for (int i = 0 ; i < rows; ++i) {
+			
+			for (int j = 0 ; j < columns; ++j) {
+				
+				if(i == j){
+					diagonalSum += list.get(i).get(j);
+					System.out.print(list.get(i).get(j)+"\t");
+				}else{
+					System.out.print("\t");
+				}
+			}
+			System.out.println();
+		}
+		return diagonalSum;
 	}
 }
