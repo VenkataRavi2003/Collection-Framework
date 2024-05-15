0f1da298c8aed11b2f02e7a1727a04d7373ca033
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-05-15 10:04:50 +0530

    Add files via upload

---
+++ DiagonalSumOfMatrix.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+public class DiagonalSumOfMatrix{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("\nEnter no.of rows : ");
+		int rows = sc.nextInt();
+
+		System.out.print("\nEnter no.of columns : ");
+		int columns = sc.nextInt();
+
+		List<List<Integer>> list = new ArrayList<>();
+
+		System.out.println("\nEnter the elements : ");
+		takeInput(list,rows,columns,sc);
+
+
+	}
+	public static void takeInput(List<List<Integer>> list,int rows,int columns,Scanner sc){
+
+		for (int i = 0 ;  i < ; ) {
+			
+		}
+	}
+}
