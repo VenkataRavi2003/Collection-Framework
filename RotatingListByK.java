f03edf2b22f5fd446bfa8cca19310db4c6ab9b88
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2025-01-06 14:37:59 +0530

    Add files via upload

--- RotatingListByK.java
+++ RotatingListByK.java
@@ -1,35 +1,54 @@
 import java.util.Scanner;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+interface IOne{
+	List<Integer> rotateListByK(List<Integer> list,int k);
+}
 public class RotatingListByK{
 	public static void main(String[] args) {
+		
 		try(Scanner sc = new Scanner(System.in)){
 			System.out.print("\nEnter the size of elements you need to keep in LIST : ");
 			int size;
 			while (true) {
 				size = getValidInput(sc);
 				if(size > 0)break;
-				System.out.println("\n"+size+"-> size must be greater than zero! Try Again : ");
+				System.out.println("\n"+size+" -> size must be greater than zero!Try Again : ");
 			}
+
 			List<Integer> list = new ArrayList<Integer>();
+
 			System.out.println("\nEnter the elements into LIST : ");
 			takeInput(list,sc,size);
 
 			System.out.println("\nBefore Rotating : "+list);
 
-			System.out.println("\nEnter K : ");
+			System.out.print("\nEnter K : ");
+			int kth = getValidInput(sc);
 
-			int k = getValidInput(sc);
-			List<Integer> rotatedList = rotateListByK(list,k);
+			IOne i1 = (l,k) -> {
+                                 List<Integer> rotatedList = new ArrayList<Integer>(l);
+                                 int n = l.size();
+                                 k = k % n;
 
-			System.out.println("\nAfter Rotating "+rotatedList);
+                                 for (int i = 0 ; i < n ; ++i) {
+                                 	
+                                 	rotatedList.set((i + k) % n , l.get(i));
+                                 }
+
+                                 return rotatedList;
+			                   };
+
+			List<Integer> res = i1.rotateListByK(list,kth);
+
+			System.out.println("\nAfter Rotating : "+res);
 
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	public static void takeInput(List<Integer> list,Scanner sc, int size){
+	public static void takeInput(List<Integer> list,Scanner sc,int size){
 		for (int i = 0 ; i < size ; ++i) {
 			list.add(getValidInput(sc));
 		}
@@ -39,19 +58,9 @@ public class RotatingListByK{
 			try{
 				return sc.nextInt();
 			}catch (InputMismatchException e) {
-				System.out.println("\nInvalid Input!Please enter valid integer as input : ");
+				System.out.println("\nInvalid Input!Please Enter valid integer as input!Try again : ");
 				sc.next();
 			}
 		}
-	}
-	public static List<Integer> rotateListByK(List<Integer> list,int k){
-		int n = list.size();
-
-		List<Integer> rotatedList = new ArrayList<>(list);
-
-		for (int i = 0 ; i < n ; ++i) {
-			rotatedList.set((i + k) % n , list.get(i));
-		}
-		return rotatedList;
 	}
 }
